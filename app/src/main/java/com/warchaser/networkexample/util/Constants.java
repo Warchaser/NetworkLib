a0bf6e5ae3ad92a168f2781425a884fe78c11685
achan <wu.shb@reachauto.com>
2021-07-08 14:06:29 +0800

    fix # Latest Configuration. Signed-off-by: achan <wu.shb@reachauto.com>

--- app/src/main/java/com/warchaser/networkexample/util/Constants.java
+++ app/src/main/java/com/warchaser/networkexample/util/Constants.java
@@ -2,7 +2,7 @@ package com.warchaser.networkexample.util;
 
 public class Constants {
 
-    public static final String BASE_URL = "http://10.0.0.186:7812/SpringLearning/admin/user/";
+    public static final String BASE_URL = "http://172.16.29.204:8080/admin/user/";
 
     //Token不存在
     public static final int TOKEN_NOT_EXIST = 100105;
